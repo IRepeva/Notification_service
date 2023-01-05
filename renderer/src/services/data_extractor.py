7702c9531ddf8ac6952a13a75b2572c3bbdc303d
Irina Repeva <repeva.i@panoramik.ru>
2023-01-05 23:43:51 +0000

    Add user data extractor and url shortener

---
+++ renderer/src/services/data_extractor.py
@@ -0,0 +1,22 @@
+from http import HTTPStatus
+
+import requests
+from utils.backoff import backoff
+
+
+class UserDataExtractor:
+    def __init__(self, endpoint: str, authorization: str) -> None:
+        self.url = endpoint
+        self.headers = {'Authorization': authorization}
+
+    @backoff()
+    def get_info(self, user_id: str) -> dict:
+        response = requests.get(
+            url=f"{self.url}/{user_id}",
+            headers=self.headers
+        )
+        if response.status_code != HTTPStatus.OK:
+            raise Exception(
+                f"Status code {response.status_code} {response.text}"
+            )
+        return response.json()
---
+++ renderer/src/services/url_shortener.py
@@ -0,0 +1,31 @@
+import requests
+from core.settings import settings
+from requests.structures import CaseInsensitiveDict
+
+
+class UrlShortener:
+    def __init__(self):
+        self.url = settings.shortener.endpoint
+        self.token = settings.shortener.token.get_secret_value()
+        self.domain = settings.shortener.domain
+
+    def _get_headers(self):
+        headers = CaseInsensitiveDict()
+        headers["Accept"] = "application/json"
+        headers["Authorization"] = f"Bearer {self.token}"
+        headers["Content-Type"] = "application/json"
+        return headers
+
+    def _get_data(self, url):
+        data = {
+            "domain": self.domain,
+            "long_url": url
+        }
+        return data
+
+    def get_short_url(self, url):
+        headers = self._get_headers()
+        data = self._get_data(url)
+        resp = requests.post(self.url, headers=headers, json=data)
+        return resp.json()['link']
+
